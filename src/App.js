fbfc791988966be7a0ca7b1b01283216036fc859
JElandorr <jordanouzunov@abv.bg>
2023-09-16 19:43:26 +0300

    update project news

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
     return (
-        <div className="App">
-            <h1>App</h1>
-        </div>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
