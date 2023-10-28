5241dbbad6a0bc3fee6ee2e683f5027a9a719a13
JElandorr <jordanouzunov@abv.bg>
2023-10-28 16:13:16 +0300

    update CreateArticle

--- src/components/articles/CreateArticle.js
+++ src/components/articles/CreateArticle.js
@@ -103,6 +103,55 @@ const CreateArticle = () => {
                                     </div>
                                     <div className="col-12">
                                         <div className="billing-info mb-20">
+                                            <label>Tags</label>
+                                            {article?.tags && (
+                                                <div className="row">
+                                                    {article?.tags?.map((tag, key) => {
+                                                        const tagExists = article.tags.includes(tag);
+                                                        console.log(tag);
+                                                        console.log(key);
+                                                        return (
+                                                            <div
+                                                                className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12 category-holder`}
+                                                                key={key}
+                                                                onClick={() => toggleSelectCategory(tag)}
+                                                            >
+                                                                {tagExists ? (
+                                                                    <>
+                                                                        <div>
+                                                                            <i class="fa-regular fa-rectangle-xmark"></i>
+                                                                        </div>
+                                                                    </>
+                                                                ) : (
+                                                                    ""
+                                                                )}
+                                                                <div className={`col-8`}>{tag}</div>
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
+                                            )}
+                                            <div className="row add-image">
+                                                <div className="col-lg-10 col-md-10">
+                                                    <input
+                                                        className="add-image-input"
+                                                        id="images"
+                                                        type="text"
+                                                        name="images"
+                                                        placeholder="Article Tags"
+                                                        value={newImageUrl}
+                                                        onChange={handleImageChange}
+                                                    />
+                                                </div>
+                                                <div
+                                                    className="col-lg-2 col-md-2 add-image-btn"
+                                                    onClick={handleImageSubmit}
+                                                >
+                                                    Add Image
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="billing-info mb-20">
                                             <label>Article Categories</label>
                                             {categories && (
                                                 <div className="row">
@@ -112,7 +161,16 @@ const CreateArticle = () => {
                                                         return (
                                                             <div
                                                                 className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12 category-holder`}
-                                                                style={isSelected ? { backgroundColor: "#b3b3b3" } : {}}
+                                                                style={
+                                                                    isSelected
+                                                                        ? {
+                                                                              backgroundColor: "#b3b3b3",
+                                                                              userSelect: "none",
+                                                                          }
+                                                                        : {
+                                                                              userSelect: "none",
+                                                                          }
+                                                                }
                                                                 key={key}
                                                                 onClick={() => toggleSelectCategory(category)}
                                                             >
@@ -168,88 +226,6 @@ const CreateArticle = () => {
                                                     })}
                                                 </div>
                                             )}
-                                            {/* <div className="row">
-                                                <div className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12`}>
-                                                    <Link
-                                                        to={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                    >
-                                                        <img
-                                                            src={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                            alt="article"
-                                                        />
-                                                    </Link>
-                                                </div>
-                                                <div className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12`}>
-                                                    <Link
-                                                        to={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                    >
-                                                        <img
-                                                            src={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                            alt="article"
-                                                        />
-                                                    </Link>
-                                                </div>
-                                                <div className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12`}>
-                                                    <Link
-                                                        to={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                    >
-                                                        <img
-                                                            src={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                            alt="article"
-                                                        />
-                                                    </Link>
-                                                </div>
-                                                <div className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12`}>
-                                                    <Link
-                                                        to={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                    >
-                                                        <img
-                                                            src={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                            alt="article"
-                                                        />
-                                                    </Link>
-                                                </div>
-                                                <div className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12`}>
-                                                    <Link
-                                                        to={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                    >
-                                                        <img
-                                                            src={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                            alt="article"
-                                                        />
-                                                    </Link>
-                                                </div>
-                                                <div className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12`}>
-                                                    <Link
-                                                        to={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                    >
-                                                        <img
-                                                            src={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                            alt="article"
-                                                        />
-                                                    </Link>
-                                                </div>
-                                                <div className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12`}>
-                                                    <Link
-                                                        to={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                    >
-                                                        <img
-                                                            src={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                            alt="article"
-                                                        />
-                                                    </Link>
-                                                </div>
-                                                <div className={`col-lg-3 col-md-4 col-sm-6 col-xxs-12`}>
-                                                    <Link
-                                                        to={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                    >
-                                                        <img
-                                                            src={`https://euphoriatravel.bg/wp-content/uploads/2022/10/bridge-6320235_1280-e1668595627741-750x440.jpg`}
-                                                            alt="article"
-                                                        />
-                                                    </Link>
-                                                </div>
-                                            </div> */}
                                             <div className="row add-image">
                                                 <div className="col-lg-10 col-md-10">
                                                     <input
@@ -316,76 +292,6 @@ const CreateArticle = () => {
                                 </div>
                             </div>
                         </div>
-
-                        {/* <div className="col-lg-5">
-                            <div className="your-order-area">
-                                <h3>Your order</h3>
-                                <div className="your-order-wrap gray-bg-4">
-                                    <div className="your-order-product-info">
-                                        <div className="your-order-top">
-                                            <ul>
-                                                <li>Product</li>
-                                                <li>Total</li>
-                                            </ul>
-                                        </div>
-                                        <div className="your-order-middle">
-                                            <ul>
-                                                {cartItems.map((cartItem, key) => {
-                                                    const discountedPrice = getDiscountPrice(
-                                                        cartItem.price,
-                                                        cartItem.discount
-                                                    );
-                                                    const finalProductPrice = (
-                                                        cartItem.price * currency.currencyRate
-                                                    ).toFixed(2);
-                                                    const finalDiscountedPrice = (
-                                                        discountedPrice * currency.currencyRate
-                                                    ).toFixed(2);
-
-                                                    discountedPrice != null
-                                                        ? (cartTotalPrice += finalDiscountedPrice * cartItem.quantity)
-                                                        : (cartTotalPrice += finalProductPrice * cartItem.quantity);
-                                                    return (
-                                                        <li key={key}>
-                                                            <span className="order-middle-left">
-                                                                {cartItem.name} X {cartItem.quantity}
-                                                            </span>{" "}
-                                                            <span className="order-price">
-                                                                {discountedPrice !== null
-                                                                    ? currency.currencySymbol +
-                                                                      (
-                                                                          finalDiscountedPrice * cartItem.quantity
-                                                                      ).toFixed(2)
-                                                                    : currency.currencySymbol +
-                                                                      (finalProductPrice * cartItem.quantity).toFixed(
-                                                                          2
-                                                                      )}
-                                                            </span>
-                                                        </li>
-                                                    );
-                                                })}
-                                            </ul>
-                                        </div>
-                                        <div className="your-order-bottom">
-                                            <ul>
-                                                <li className="your-order-shipping">Shipping</li>
-                                                <li>Free shipping</li>
-                                            </ul>
-                                        </div>
-                                        <div className="your-order-total">
-                                            <ul>
-                                                <li className="order-total">Total</li>
-                                                <li>{currency.currencySymbol + cartTotalPrice.toFixed(2)}</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div className="payment-method"></div>
-                                </div>
-                                <div className="place-order mt-25">
-                                    <button className="btn-hover">Place Order</button>
-                                </div>
-                            </div>
-                        </div> */}
                     </div>
                 </div>
             </div>
